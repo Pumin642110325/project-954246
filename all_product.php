a26f445d504cde2b3f54dab391f4bef78f541d9c
Pumin642110325 <pumin_chummeungyen@cmu.ac.th>
2025-02-16 15:56:19 +0700

    update1

---
+++ all_product.php
@@ -0,0 +1,18 @@
+<?php
+    require_once "header.php";
+    ?>
+    <div class="content">
+    <div class="tablefirst">
+       <table>
+          <tr>
+             <td>All Product</td>
+          </tr>
+       </table>
+    </div>
+ </div>
+ <?php
+    require_once "best.php";
+    require_once "sale.php";
+?>
+    </body>
+    </html>
---
+++ sale.php
@@ -0,0 +1,44 @@
+<?php
+   require_once "header.php";
+?>
+<html>
+    <body>
+    <div class="content">
+         <div class="tablefirst">
+            <table>
+               <tr>
+                  <td>Sell product</td>
+               </tr>
+            </table>
+         </div>
+      </div>
+    <div class="content">
+    <div class="product-list">
+        <div class="product">
+            <img src="Gusta-Brookie-1DCE-MKII-กีตาร์โปร่งไฟฟ้า-300x300.jpg" alt="Product 1" class="product-image">
+            <h3>Gusta Brookie</h3>
+            <h5>********** -12% **********</h5>
+            <p>Gusta Brookie 2D กีตาร์โปร่ง/โปร่งไฟฟ้า Acoustic Guitar</p>
+            <p>Price: ฿24,900</p>
+            <a href="product-detail.php?id=1">View Details</a>
+        </div>
+        <div class="product">
+            <img src="Cover-คีย์บอร์ด-Roland-XPS-30-300x300.jpg" alt="Product 2" class="product-image">
+            <h3>Roland-XPS-30 </h3>
+            <h5>********** -20% **********</h5>
+            <p>Roland XPS-30 คีย์บอร์ดไฟฟ้า Electronic Keyboard</p>
+            <p>Price: ฿10,900</p>
+            <a href="product-detail.php?id=2">View Details</a>
+        </div>
+        <div class="product">
+            <img src="CMC-Remix-2023-กลองชุด-300x300.jpg" alt="Product 3" class="product-image">
+            <h3>CMC-Remix</h3>
+            <h5>********** -6% **********</h5>
+            <p>CMC Remix 2023 กลองชุด Acoustic Drums</p>
+            <p>Price: ฿17900</p>
+            <a href="product-detail.php?id=3">View Details</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
